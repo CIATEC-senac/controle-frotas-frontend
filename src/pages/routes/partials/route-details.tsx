11fc843c9d3c56b88ca8455382177b1d66359815
Andreina Palheta <palhetaandreina@gmail.com>
2025-04-18 19:35:01 -0300

    feat: Adicionado componente de status

---
+++ src/pages/routes/partials/route-details.tsx
@@ -0,0 +1,108 @@
+import { Clock, ExternalLink, MapPin } from 'lucide-react';
+
+import { EditButton } from '@/components/layout/edit-button';
+import { Button } from '@/components/ui/button';
+import { getEstimatedArrivalDate, getName, Route } from '@/models/route.type';
+import { ColumnDef } from '@tanstack/react-table';
+import { Link } from 'react-router';
+import { DeleteRouteDialog } from '../delete-dialog';
+import { FormDialog } from '../form-dialog';
+
+export const RoutePathTracking = ({ route }: { route: Route }) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex gap-3 items-center">
+        <MapPin size={14} />
+        <span>{route.path.origin.toUpperCase()}</span>
+      </div>
+
+      <div className="flex gap-3 items-center">
+        <MapPin size={14} />
+        <span>{route.path.destination.toUpperCase()}</span>
+      </div>
+    </div>
+  );
+};
+
+export const RouteDepartureArrival = ({ route }: { route: Route }) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex gap-3 items-center">
+        <Clock size={14} />
+        <span>Saída: {route.startAt}</span>
+      </div>
+
+      <div className="flex gap-3 items-center">
+        <Clock size={14} />
+        <span>Chegada: {getEstimatedArrivalDate(route)}</span>
+      </div>
+    </div>
+  );
+};
+
+export const RouteActions = ({ route }: { route: Route }) => {
+  return (
+    <div className="flex gap-2 justify-end">
+      <FormDialog title="Editar Rota" trigger={<EditButton />} route={route} />
+      <DeleteRouteDialog route={route} />
+    </div>
+  );
+};
+
+export const RouteLink = ({ route }: { route: Route }) => {
+  return (
+    <Button variant="link" asChild>
+      <Link to={`/route/${route.id}`}>
+        {getName(route)} <ExternalLink />
+      </Link>
+    </Button>
+  );
+};
+
+export const RouteStatus = ({ status }: { status: boolean }) => {
+  if (status) {
+    return (
+      <span className="text-green-600 bg-green-100 rounded-2xl py-1 px-3">
+        Ativo
+      </span>
+    );
+  }
+
+  return (
+    <span className="text-gray-600 bg-gray-100 rounded-2xl py-1 px-3">
+      Inativo
+    </span>
+  );
+};
+
+export const columns: ColumnDef<Route>[] = [
+  {
+    header: 'Nome',
+    cell: ({ row }) => <RouteLink route={row.original} />,
+  },
+  {
+    header: 'Origem / Destino',
+    cell: ({ row }) => <RoutePathTracking route={row.original} />,
+  },
+  {
+    header: 'Paradas',
+    cell: ({ row }) => `${row.original.path.stops.length} parada(s)`,
+  },
+  {
+    header: 'Horários',
+    cell: ({ row }) => <RouteDepartureArrival route={row.original} />,
+  },
+  {
+    header: 'Distância',
+    cell: ({ row }) =>
+      `${(row.original.estimatedDistance / 1000.0).toFixed(2)} Km`,
+  },
+  {
+    header: 'Status',
+    cell: ({ row }) => <RouteStatus status={row.original.status} />,
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <RouteActions route={row.original} />,
+  },
+];
--- src/pages/routes/routes.tsx
+++ src/pages/routes/routes.tsx
@@ -1,82 +1,19 @@
-import { ColumnDef } from '@tanstack/react-table';
-import { Clock, MapPin, Search } from 'lucide-react';
+import { Search } from 'lucide-react';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 import { CreateButton } from '@/components/layout/create-button';
 import { DataTable } from '@/components/layout/data-table';
-import { EditButton } from '@/components/layout/edit-button';
 import { FetchError } from '@/components/layout/fetch-error';
 import { Layout } from '@/components/layout/layout';
 import { LoadingMessage } from '@/components/layout/loading-message';
 import { TextField } from '@/components/layout/textfield';
 import { API } from '@/lib/api';
-import { getEstimatedArrivalDate, getName, Route } from '@/models/route.type';
 
-import { DeleteRouteDialog } from './delete-dialog';
+import { normalizeString } from '@/lib/normalize';
+import { filter } from './filter';
 import { FormDialog } from './form-dialog';
-
-const columns: ColumnDef<Route>[] = [
-  {
-    header: 'Nome',
-    cell: ({ row }) => getName(row.original),
-  },
-  {
-    header: 'Origem / Destino',
-    cell: ({ row }) => (
-      <div className="flex flex-col gap-1">
-        <div className="flex gap-3 items-center">
-          <MapPin size={14} />
-          <span>{row.original.path.origin.toUpperCase()}</span>
-        </div>
-
-        <div className="flex gap-3 items-center">
-          <MapPin size={14} />
-          <span>{row.original.path.destination.toUpperCase()}</span>
-        </div>
-      </div>
-    ),
-  },
-  {
-    header: 'Horários',
-    cell: ({ row }) => (
-      <div className="flex flex-col gap-1">
-        <div className="flex gap-3 items-center">
-          <Clock size={14} />
-          <span>Saída: {row.original.startAt}</span>
-        </div>
-
-        <div className="flex gap-3 items-center">
-          <Clock size={14} />
-          <span>Chegada: {getEstimatedArrivalDate(row.original)}</span>
-        </div>
-      </div>
-    ),
-  },
-  {
-    header: 'Distância',
-    cell: ({ row }) =>
-      `${(row.original.estimatedDistance / 1000.0).toFixed(2)} Km`,
-  },
-  {
-    header: 'Status',
-    cell: ({ row }) => (row.original.status ? 'Ativo' : 'Inativo'),
-  },
-  {
-    id: 'actions',
-    cell: ({ row }) => (
-      <div className="flex gap-2 justify-end">
-        <FormDialog
-          title="Editar Rota"
-          trigger={<EditButton />}
-          route={row.original}
-        />
-
-        <DeleteRouteDialog route={row.original} />
-      </div>
-    ),
-  },
-];
+import { columns } from './partials/route-details';
 
 export const RoutesPage = () => {
   const [search, setSearch] = useState('');
@@ -102,7 +39,7 @@ export const RoutesPage = () => {
     return (
       <DataTable
         columns={columns}
-        data={vehicles || []}
+        data={(vehicles || []).filter(filter(normalizeString(search)))}
         empty="Nenhum resultado encontrado"
       />
     );
@@ -117,7 +54,7 @@ export const RoutesPage = () => {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             prefixIcon={<Search size={16} />}
-            placeholder="Busque por modelo ou placa..."
+            placeholder="Busque por origem, destino ou paradas..."
           />
 
           <FormDialog
